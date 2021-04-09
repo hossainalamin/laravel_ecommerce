cb87e4afa0a909fdd6c933c532811b14b3f62d5b
Md Alamin Hossain <hossainalamin980@gmail.com>
2021-04-09 12:30:00 +0600

    Login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,27 @@
 @extends('master')
 @section('content')
-<h1>login Here</h1>
-<button class="btn btn-primary">Login</button>
+<div class="continer login-form">
+    <div class="row">
+        <div class="col-md-4">
+        </div>
+        <div class="col-md-4">
+            <div class="card  rounded login">
+                <div class="card-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
-Route::view("login","login");
