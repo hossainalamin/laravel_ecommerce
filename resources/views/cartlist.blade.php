d01e4bc538cab65777c5b188c3ba07af636d2cec
Md Alamin Hossain <hossainalamin980@gmail.com>
2021-04-17 16:23:11 +0600

    Update cartlist.blade.php

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -1,30 +1,30 @@
 @extends('master')
 @section('content')
 <div class="container">
-    <div class="custom-product">
+    <div class="cart-product">
         <div class="cart-product my-3">
-            <h3 class="text-center font-italic my-2 bg-dark py-3 text-light">Cart Item<h3>
-                    @foreach($products as $item)
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img class="trending-image" src="{{$item->gallery}}" alt="">
+            <h3 class="text-center font-italic my-2 bg-dark py-3 text-light">Cart Item</h3>
+            @foreach($products as $item)
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="trending-image" src="{{$item->gallery}}" alt="">
+                </div>
+                <div class="col-md-6">
+                    <a href="detail/{{$item->id}}">
+                        <div>
+                            <p class="cart-item">{{$item->name}}</p>
+                            <p class="cart-item">{{$item->description}}</p>
                         </div>
-                        <div class="col-md-6">
-                            <a href="detail/{{$item->id}}">
-                                <div>
-                                    <p class="cart-item">{{$item->name}}</p>
-                                    <p class="cart-item">{{$item->description}}</p>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-md-3">
-                            <button class="btn btn-outline-warning">Remove</button>
-                            <button class="btn btn-outline-success">Checkout</button>
-                        </div>
-                        <span class="br-3"></span>
-                    </div>
-                    <hr>
-                    @endforeach
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a href="/removecart/{{$item->cart_id}}" class="btn btn-outline-warning">Remove</a>
+                </div>
+                <span class="br-3"></span>
+            </div>
+            <hr>
+            @endforeach
+            <a href="/ordernow" class="btn btn-success my-3">Checkout</a>
         </div>
     </div>
 </div>
